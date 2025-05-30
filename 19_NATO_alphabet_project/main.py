be571558dc3083331d1cae3b7794b907bee446ed
Tetiana Niehrieieva <tetiana.niehrieieva@itomy.ch>
2025-05-30 10:39:41 +0300

    Handled KeyError exceptions to prevent crashes

--- 19_NATO_alphabet_project/main.py
+++ 19_NATO_alphabet_project/main.py
@@ -2,12 +2,15 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_data_frame = pandas.DataFrame(data)
-
-# Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-word = input("Enter a word: ").upper()
+# alphabet_dict => NATO dictionary in format: {"A": "Alfa", "B": "Bravo"}
 alphabet_dict = {row.letter:row.code for (index, row) in alphabet_data_frame.iterrows()}
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [alphabet_dict[letter] for letter in word if alphabet_dict[letter]]
+        print(output_list)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
 
-# Create a list of the phonetic code words from a word that the user inputs.
-output_list = [alphabet_dict[letter] for letter in word if alphabet_dict[letter]]
-print(output_list)
+generate_phonetic()
